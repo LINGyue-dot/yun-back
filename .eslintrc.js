ae85bdbd045b4818f9a37534e0589ec025c3d88e
LINGyue-dot <1635679571@qq.com>
2020-08-24 00:00:37 +0800

    2

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
